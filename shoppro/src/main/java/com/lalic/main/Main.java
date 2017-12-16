38ebbcaf0b227dd1098df76891b0e1802fd3b5ce
Lalic <517946087@qq.com>
2017-12-16 23:06:21 +0800

    main

---
+++ shoppro/src/main/java/com/lalic/main/Main.java
@@ -0,0 +1,19 @@
+package com.lalic.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(value = {"com.lalic.controller", "com.lalic.iml"})
+@EntityScan("com.lalic.model")
+@EnableJpaRepositories("com.lalic.dao")
+public class Main {
+    public static void main(String[] args) {
+        //5655
+        SpringApplication.run(Main.class, args);
+    }
+
+}
